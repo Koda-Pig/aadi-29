0090a273db4fac2ef26f02cd3269ad04291c970a
Josh Koter <josh.koter@gmail.com>
2025-04-25 16:22:30 +0200

    ...

--- src/generateHash.ts
+++
@@ -1,6 +0,0 @@
-import { SHA256 } from "crypto-js";
-
-const password = "goodbye twenties";
-const hash = SHA256(password).toString();
-console.log("Password:", password);
-console.log("SHA-256 Hash:", hash);
