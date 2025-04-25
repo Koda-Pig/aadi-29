befff2abb6d6b3b09d2d4c2d73e2a59ef3159496
Josh Koter <josh.koter@gmail.com>
2025-04-25 18:59:48 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { ScrollProvider } from "./contexts/ScrollContext";
-import StarField from "./components/SpaceBackground";
-import styles from "./styles/app.module.scss";
 import { PasswordProtection } from "./components/PasswordProtection";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./pages/Gallery";
@@ -21,15 +17,6 @@ function App() {
   return (
     <Router>
       <ScrollProvider>
-        {/* Fixed background container */}
-        <div className={styles.background}>
-          <Canvas camera={{ position: [0, 0, 1] }} className={styles.canvas}>
-            <ambientLight intensity={0.1} />
-            <StarField />
-            <OrbitControls enableZoom={false} enablePan={false} />
-          </Canvas>
-        </div>
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />} />
