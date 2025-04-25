5f03bb469e0ae4252a4ac52bba0b45e8364f806b
Josh Koter <josh.koter@gmail.com>
2025-04-25 11:16:07 +0200

    ...

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -4,14 +4,9 @@ export const useScrollPosition = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
-    const updatePosition = () => {
-      setScrollPosition(window.scrollY);
-      console.log(window.scrollY);
-    };
-
+    const updatePosition = () => setScrollPosition(window.scrollY);
     window.addEventListener("scroll", updatePosition);
     updatePosition();
-
     return () => window.removeEventListener("scroll", updatePosition);
   }, []);
 
