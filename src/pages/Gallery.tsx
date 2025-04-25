326203bb3310af90f7d63730417643f0015b3833
Josh Koter <josh.koter@gmail.com>
2025-04-25 19:19:32 +0200

    ...

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,17 +1,11 @@
 import { Canvas } from "@react-three/fiber";
-import { ScrollControls, OrbitControls } from "@react-three/drei";
-import CosmicTimeline from "../components/CosmicTimeline";
 import styles from "../styles/gallery.module.scss";
 
 const Gallery = () => {
   return (
     <div className={styles.gallery}>
       <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <ScrollControls pages={3} damping={0.1}>
-          <ambientLight intensity={0.1} />
-          <CosmicTimeline />
-        </ScrollControls>
-        <OrbitControls enableZoom={false} enablePan={false} />
+        <></>
       </Canvas>
     </div>
   );
