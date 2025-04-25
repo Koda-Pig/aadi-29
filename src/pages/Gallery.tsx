35d92a7dcad6ebc9cd63cbd98e923f4dc4264055
Josh Koter <josh.koter@gmail.com>
2025-04-25 19:29:45 +0200

    ...

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { ScrollControls } from "@react-three/drei";
-import ImageTunnel from "../components/ImageTunnel";
 import LoadingSpinner from "../components/LoadingSpinner";
 import styles from "../styles/gallery.module.scss";
 
@@ -9,21 +6,9 @@ const Gallery = () => {
   const [loadingProgress, setLoadingProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
-  const handleProgress = (progress: number) => {
-    setLoadingProgress(progress);
-    if (progress === 100) {
-      setIsLoading(false);
-    }
-  };
-
   return (
     <div className={styles.gallery}>
       {isLoading && <LoadingSpinner progress={loadingProgress} />}
-      <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-        <ScrollControls pages={8} damping={0.3}>
-          <ImageTunnel onLoadProgress={handleProgress} />
-        </ScrollControls>
-      </Canvas>
     </div>
   );
 };
